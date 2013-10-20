6c12b33a7e03037a6d319174fedd0b4c840bcc07
Benjamin Clark <ben@benclark.com>
2013-10-20 15:39:44 -0400

    Fixes to drush_vget (formerly drush_vget_json) in helper library.

--- libraries/helper.rb
+++ libraries/helper.rb
@@ -33,19 +33,28 @@ module DrushHelper
   def self.drush_vget_json(path, name, uri = 'http://default', exact_match = true)
     exact_option = "--exact" if exact_match
     cmd = "#{drush_which} -l #{uri} -r #{path} vget #{name} #{exact_option} --format=json"
-    Chef::Log.debug("drush_vget_json: #{cmd}")
+    Chef::Log.debug("drush_vget: #{cmd}")
     begin
       p = shell_out!(cmd)
-      JSON.parse(p.stdout) if p.stdout
+      JSON.parse(p.stdout, { :symbolize_names => false }) if p && p.stdout
     rescue => e
-      Chef::Log.debug("drush_vget_json: #{e.message}")
+      Chef::Log.debug("drush_vget: #{e.message}")
     end
   end
 
   def self.drush_vget_match?(path, name, value, uri = 'http://default')
     vget = drush_vget_json(path, name, uri)
+    # I know this looks crazy, but it's due to the escaped string that
+    # `drush --format=json` returns.
+    vget = JSON.generate(vget)
+    # Convert the value to JSON for later string comparison.
     value = value.to_s if value.is_a?(Integer)
-    vget.is_a?(Hash) && vget.key?(name) && vget[name] == value
+    value_json = JSON.generate({ name => value })
+
+    Chef::Log.debug("drush_vget_match?: value = #{value_json.to_s}")
+    Chef::Log.debug("drush_vget_match?:  vget = #{vget.to_s}")
+
+    !vget.nil? && vget.to_s == value_json.to_s
   end
 
   def self.drush_which
